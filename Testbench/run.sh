4e6b6dd6a0d67f17e9811404120efb16bfb3f9cc
Amr El Batarny <amrelbatarny8@gmail.com>
2025-04-08 01:36:11 +0200

    Migrated the CRV and coverage from cocotb-coverage to PyVSC and added test_apb_wrapper.py for pytest & cocotb-test workflow

--- Testbench/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Clean previous simulation files
-echo "Cleaning previous simulation files..."
-rm -rf __pycache__ modelsim.ini transcript vsim.wlf sim_build
-
-# Run make to build the simulation
-echo "Running Makefile..."
-make
