91582ab5004568e30b7b045f3943b9123b343f40
Amr El Batarny <amrelbatarny8@gmail.com>
2025-04-26 08:50:21 +0300

    Now setup.tcl includes commands for exporting PyQuesta's coverage reports

--- Testbench/setup.tcl
+++ Testbench/setup.tcl
@@ -6,23 +6,26 @@ sim:/APB_Wrapper/PADDR \
 sim:/APB_Wrapper/PWRITE \
 sim:/APB_Wrapper/PWDATA \
 sim:/APB_Wrapper/PENABLE \
+sim:/APB_Wrapper/PSELx  \
+sim:/APB_Wrapper/PSTRB \
 sim:/APB_Wrapper/PRDATA \
-sim:/APB_Wrapper/PSLVERR \
 sim:/APB_Wrapper/PREADY \
-sim:/APB_Wrapper/RegWRITE \
-sim:/APB_Wrapper/RegENABLE \
-sim:/APB_Wrapper/RegADDR \
-sim:/APB_Wrapper/RegWDATA \
-sim:/APB_Wrapper/RegRDATA \
-sim:/APB_Wrapper/RegREADY
+sim:/APB_Wrapper/RegisterFile_inst/write_en \
+sim:/APB_Wrapper/RegisterFile_inst/read_en \
+sim:/APB_Wrapper/RegisterFile_inst/addr \
+sim:/APB_Wrapper/RegisterFile_inst/wdata \
+sim:/APB_Wrapper/RegisterFile_inst/rdata \
+sim:/APB_Wrapper/RegisterFile_inst/byte_strobe \
+sim:/APB_Wrapper/RegisterFile_inst/mask \
 
 # Current and Next States Waveforms
 add wave -position insertpoint  \
--color cyan sim:/APB_Wrapper/apb_slave/NextState \
--color cyan sim:/APB_Wrapper/apb_slave/CurrentState
+-color cyan sim:/APB_Wrapper/APB_Slave_inst/next_state \
+-color cyan sim:/APB_Wrapper/APB_Slave_inst/current_state
 
 # Assertions
 add wave -position insertpoint \
+sim:/APB_Wrapper/APB_SVA_inst/PSTRB_low_in_read_transfer \
 sim:/APB_Wrapper/APB_SVA_inst/PWRITE_never_X \
 sim:/APB_Wrapper/APB_SVA_inst/PENABLE_never_X \
 sim:/APB_Wrapper/APB_SVA_inst/PREADY_never_X \
@@ -32,40 +35,51 @@ sim:/APB_Wrapper/APB_SVA_inst/PADDR_stable_in_transfer \
 sim:/APB_Wrapper/APB_SVA_inst/PWRITE_stable_in_transfer \
 sim:/APB_Wrapper/APB_SVA_inst/PENABLE_stable_in_transfer \
 sim:/APB_Wrapper/APB_SVA_inst/PWDATA_stable_in_wr_transfer \
-sim:/APB_Wrapper/APB_SVA_inst/PSLVERR_stable_in_transfer
 
 # Registers Waveform
 add wave -position insertpoint  \
--color gold sim:/APB_Wrapper/reg_file/SYS_STATUS_REG \
--color gold sim:/APB_Wrapper/reg_file/INT_CTRL_REG \
--color gold sim:/APB_Wrapper/reg_file/DEV_ID_REG \
--color gold sim:/APB_Wrapper/reg_file/MEM_CTRL_REG \
--color gold sim:/APB_Wrapper/reg_file/TEMP_SENSOR_REG \
--color gold sim:/APB_Wrapper/reg_file/ADC_CTRL_REG \
--color gold sim:/APB_Wrapper/reg_file/DBG_CTRL_REG \
--color gold sim:/APB_Wrapper/reg_file/GPIO_DATA_REG \
--color gold sim:/APB_Wrapper/reg_file/DAC_OUTPUT_REG \
--color gold sim:/APB_Wrapper/reg_file/VOLTAGE_CTRL_REG \
--color gold sim:/APB_Wrapper/reg_file/CLK_CONFIG_REG \
--color gold sim:/APB_Wrapper/reg_file/TIMER_COUNT_REG \
--color gold sim:/APB_Wrapper/reg_file/INPUT_DATA_REG \
--color gold sim:/APB_Wrapper/reg_file/OUTPUT_DATA_REG \
--color gold sim:/APB_Wrapper/reg_file/DMA_CTRL_REG \
--color gold sim:/APB_Wrapper/reg_file/SYS_CTRL_REG
+-color gold sim:/APB_Wrapper/RegisterFile_inst/SYS_STATUS_REG \
+-color gold sim:/APB_Wrapper/RegisterFile_inst/INT_CTRL_REG \
+-color gold sim:/APB_Wrapper/RegisterFile_inst/DEV_ID_REG \
+-color gold sim:/APB_Wrapper/RegisterFile_inst/MEM_CTRL_REG \
+-color gold sim:/APB_Wrapper/RegisterFile_inst/TEMP_SENSOR_REG \
+-color gold sim:/APB_Wrapper/RegisterFile_inst/ADC_CTRL_REG \
+-color gold sim:/APB_Wrapper/RegisterFile_inst/DBG_CTRL_REG \
+-color gold sim:/APB_Wrapper/RegisterFile_inst/GPIO_DATA_REG \
+-color gold sim:/APB_Wrapper/RegisterFile_inst/DAC_OUTPUT_REG \
+-color gold sim:/APB_Wrapper/RegisterFile_inst/VOLTAGE_CTRL_REG \
+-color gold sim:/APB_Wrapper/RegisterFile_inst/CLK_CONFIG_REG \
+-color gold sim:/APB_Wrapper/RegisterFile_inst/TIMER_COUNT_REG \
+-color gold sim:/APB_Wrapper/RegisterFile_inst/INPUT_DATA_REG \
+-color gold sim:/APB_Wrapper/RegisterFile_inst/OUTPUT_DATA_REG \
+-color gold sim:/APB_Wrapper/RegisterFile_inst/DMA_CTRL_REG \
+-color gold sim:/APB_Wrapper/RegisterFile_inst/SYS_CTRL_REG
 
 .vcop Action toggleleafnames
 
-coverage save -onexit coverage.ucdb
+# Saving coverage database in UCDB file for PyVSC
+# coverage save -onexit ../Coverage_Reports/Exported_by_PyVSC/apb_coverage.ucdb
 
+# Saving coverage database in UCDB file for PyQuesta
+# coverage save -onexit ../Coverage_Reports/Exported_by_PyQuesta/apb_coverage.ucdb
 
 run -all
 
+# quit -sim
+
+# ====================== PyVSC collected coverage ======================
+
 # Generate a TXT Coverage Report
-# vcover report apb_coverage.ucdb -details -annotate -all -output ../Coverage_Reports/Exported_by_PyVSC/coverage_report.txt
+# vcover report ../Coverage_Reports/Exported_by_PyVSC/apb_coverage.ucdb -details -annotate -all -output ../Coverage_Reports/Exported_by_PyVSC/coverage_report.txt
 
 # Generate an HTML Coverage Report
 # vcover report -html -output ../Coverage_Reports/Exported_by_PyVSC/coverage_report_by_questasim -details -annotate ../Coverage_Reports/Exported_by_PyVSC/apb_coverage.ucdb
 
-#quit -sim
 
-#vcover report -html -output test_coverage -details -annotate coverage.ucdb
+# ===================== PyQuesta collected coverage =====================
+
+# Generate a TXT Coverage Report
+# vcover report ../Coverage_Reports/Exported_by_PyQuesta/apb_coverage.ucdb -details -annotate -all -output ../Coverage_Reports/Exported_by_PyQuesta/coverage_report.txt
+
+# Generate an HTML Coverage Report
+# vcover report -html -output ../Coverage_Reports/Exported_by_PyQuesta/coverage_report_by_questasim -details -annotate ../Coverage_Reports/Exported_by_PyQuesta/apb_coverage.ucdb
