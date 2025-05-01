53f0a8b230a1d22877182ff8bbcef7946ea5e937
Amr El Batarny <amrelbatarny8@gmail.com>
2025-05-01 12:01:23 +0300

    share SEQR across the TB if needed

--- Testbench/Agent.py
+++ Testbench/Agent.py
@@ -8,6 +8,7 @@ from pyuvm import (
     uvm_agent,
     uvm_sequencer,
     uvm_analysis_port,
+    ConfigDB,
 )
 from Driver import ApbDriver
 from Monitor import ApbMonitor
@@ -19,6 +20,9 @@ class ApbAgent(uvm_agent):
 		self.drv = ApbDriver.create("drv", self)
 		self.mon = ApbMonitor.create("mon", self)
 		self.agt_ap = uvm_analysis_port.create("agt_ap", self)
+
+		# share SEQR across the TB if needed
+		ConfigDB().set(None, "*", "SQR", self.sqr)
 
 	def connect_phase(self):
 		self.drv.seq_item_port.connect(self.sqr.seq_item_export)
