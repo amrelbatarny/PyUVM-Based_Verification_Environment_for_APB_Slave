494c03b91b7de67610fe6d40ef2e52f64a519764
Amr El Batarny <amrelbatarny8@gmail.com>
2025-05-03 21:02:01 +0300

    Handled the three configuration parameters in the environment's build phase

--- Testbench/Environment.py
+++ Testbench/Environment.py
@@ -12,27 +12,56 @@ from pyuvm import (
     uvm_env,
     ConfigDB,
 )
-from pyuvm import UVMConfigItemNotFound
+from pyuvm import uvm_factory, UVMConfigItemNotFound, UVMFatalError
 
 from Agent          import ApbAgent
 from Coverage       import ApbCoverage
 from Scoreboard     import ApbScoreboard
 from Adapter        import ApbRegAdapter
 from RegisterBlock  import ApbRegBlock
+from SequenceItemVSC import ApbSeqItemVSC
+from SequenceItemCR import ApbSeqItemCR
+from SequenceItemCCVG import ApbSeqItemCCVG
 
 
 class ApbEnv(uvm_env):
 
 	def build_phase(self):
 		try:
-			self.sv_coverage_en = ConfigDB().get(self, "", "ENABLE_SV_COVERAGE")
+			self.sv_coverage_en 	= ConfigDB().get(self, "", "ENABLE_SV_COVERAGE")
+			self.vsc_coverage_en 	= ConfigDB().get(self, "", "ENABLE_VSC_COVERAGE")
+			self.sv_rand_en 		= ConfigDB().get(self, "", "ENABLE_SV_RANDOMIZATION")
+			self.vsc_rand_en 		= ConfigDB().get(self, "", "ENABLE_VSC_RANDOMIZATION")
+			self.cr_rand_en 		= ConfigDB().get(self, "", "ENABLE_CR_RANDOMIZATION")
+			self.ccvg_rand_en 		= ConfigDB().get(self, "", "ENABLE_CCVG_RANDOMIZATION")
 		except UVMConfigItemNotFound:
-			self.sv_coverage_en = False
+			self.sv_coverage_en 	= False
+			self.vsc_coverage_en 	= False
+			self.vsc_rand_en 		= False
+			self.cr_rand_en 		= False
+			self.ccvg_rand_en 		= False
 
-		try:
-			self.vsc_coverage_en = ConfigDB().get(self, "", "ENABLE_VSC_COVERAGE")
-		except UVMConfigItemNotFound:
-			self.vsc_coverage_en = False
+		# Exactly one mode must be true
+		if	 self.sv_rand_en:
+			# Bypass Python-side randomization
+			pass
+		elif self.vsc_rand_en and not self.cr_rand_en and not self.ccvg_rand_en:
+			# Default: using ApbSeqItemVSC, no override needed
+			pass
+		elif not self.vsc_rand_en and self.cr_rand_en and not self.ccvg_rand_en:
+			# Override VSC item with constrainedrandom item
+			uvm_factory().set_type_override_by_type(ApbSeqItemVSC, ApbSeqItemCR)
+		elif not self.vsc_rand_en and not self.cr_rand_en and self.ccvg_rand_en:
+			# Override VSC item with cocotb-coverage CRV item
+			uvm_factory().set_type_override_by_type(ApbSeqItemVSC, ApbSeqItemCCVG)
+		else:
+			# Invalid combination: must choose exactly one
+			raise UVMFatalError(
+				f"{self.get_type_name()}: "
+				"Exactly one randomization mode must be enabled "
+				"(VSC, CR, or CCVG). "
+				"Please check your ConfigDB settings."
+				)
 
 		self.agt = ApbAgent.create("agt", self)
 
